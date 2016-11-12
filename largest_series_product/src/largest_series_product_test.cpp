cd7570b655ca1348a3707ffdb83ffc2b80e8c907
Joan Massich <sik@visor.udg.edu>
2016-11-12 16:33:57 +0100

    [largest_series_product] add Gtest

---
+++ largest_series_product/src/largest_series_product_test.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include "largest_series_product.h"
+
+TEST (LargestSeriesProductTest, DummyGetZero)
+{
+  ASSERT_EQ (0, get_zero());
+}
+
+int main(int ac, char* av[])
+{
+  testing::InitGoogleTest(&ac, av);
+  return RUN_ALL_TESTS();
+}
