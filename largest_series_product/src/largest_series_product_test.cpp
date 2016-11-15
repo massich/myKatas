3712aa80522e4579e072d0f3b5da53457df99bb5
Joan Massich <sik@visor.udg.edu>
2016-11-15 11:28:39 +0100

    [largest_series_product] Add normal behavior tests

--- largest_series_product/src/largest_series_product_test.cpp
+++ largest_series_product/src/largest_series_product_test.cpp
@@ -12,6 +12,28 @@ TEST (LargestSeriesProductTest, TestLargestProductof2UnsortedInput)
   ASSERT_EQ (48, lsp::largest_product("576802143", 2));
 }
 
+
+TEST (LargestSeriesProductTest, TestLargestProductof3)
+{
+  ASSERT_EQ (504, lsp::largest_product( "0123456789", 3));
+}
+
+TEST (LargestSeriesProductTest, TestLargestProductof3UnsortedInput)
+{
+  ASSERT_EQ (270, lsp::largest_product("1027839564", 3));
+}
+
+TEST (LargestSeriesProductTest, TestLargestProductof5)
+{
+  ASSERT_EQ (15120, lsp::largest_product( "0123456789", 5));
+}
+
+TEST (LargestSeriesProductTest, TestBigNumber)
+{
+  auto const series = "73167176531330624919225119674426574742355349194934";
+  ASSERT_EQ (23520, lsp::largest_product( series, 6));
+}
+
 int main(int ac, char* av[])
 {
   testing::InitGoogleTest(&ac, av);
